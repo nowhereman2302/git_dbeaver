0c51effee7244adcaf92127322c8ee0d23b2ea75
nowhereman2302 <137283274+nowhereman2302@users.noreply.github.com>
2023-06-22 11:57:35 +0530

    Dev test 2 (#2)
    * new_commit
    
    * new_commit
    
    ---------
    
    Co-authored-by: L-0129 <L-0129@DESKTOP-QD2GDD3>

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-select * from albums a where AlbumId between 1 and 20;
-=======
-select * from albums a where AlbumId between 1 and 11;
->>>>>>> branch 'master' of https://github.com/nowhereman2302/git_dbeaver.git
+select * from albums a where AlbumId =1;
